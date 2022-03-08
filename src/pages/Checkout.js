07da3be781860d7f2a37595a13866aa30527b820
Anna M Attehed <anna.attehed@medieinstitutet.se>
2022-03-08 11:21:47 +0100

    before merge

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,28 +1,92 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import '../Styling/checkout.css';
+import { useEffect } from 'react'
 
-function Checkout(props) {
-    /* const { product } = props;
-    const [checkoutItems, setCheckoutItems] = useState([]);
+function Checkout({items, sum}) {
+ 
 
-    const addToCheckout = (shoes) => {
-        setCheckoutItems([...checkoutItems, product]);
-    }
-
-    const checkoutProducts = checkoutItems.map((product) => (
-        <div key={product.id}>
-          {`${product.title}: ${product.price}`}
-        </div>
-      ));
+  useEffect(() => {
     
+    console.log(items)
+    
+  }, [items])
 
-    console.log(checkoutItems); */
+  let content = (
+
+    <div className='nothing-in-checkout-page'>
+      <h2 className='go-back-text'>Nothing to submit, go back to shopping!</h2>
+      <Link to="/"><button className='back-to-homepage-btn'>Back to products </button></Link>
+      {items?.map(i => (
+        <p>ID: {i.id}</p>
+      ))}
+    </div>
+
+  )
+
+  if (items.length >= 1) {
+
+    content = (
+
+      <div className='product-parent-checkout'>
+              <div className='checkout-container'>
+
+                {items.map(shoes =>(
+                  <table info-parent-checkout>
+                    <tr className='info-in-checkout'>
+                        <th><img src={shoes.url} size='20px' alt='checkoutimage' className='checkout-img'></img>/</th>
+                        <td>< h1 className='title-checkout'>{shoes.title}</h1></td>
+                        <td>< h2 className='price-checkout'>{shoes.price} SEK</h2></td>
+                    </tr>
+                  </table>
+
+
+                ))}
+                
+                <div className='order-total'> Order summary: {sum} SEK</div>
+              </div>
+
+              <div className='input-info-container'>
+
+                    <label>Firstname</label>
+                    <input type="text" namn="firstname" className='customer-form' />
+
+                    <label>Lastname</label>
+                    <input type="text" namn="lastname" className='customer-form' />
+                    
+                    <label>Telephone</label>
+                    <input type="text" namn="phonenumber" className='customer-form' />
+
+                    <label>E-mail</label>
+                    <input type="text" namn="emailaddress" className='customer-form' />
+
+                    <label>Adress</label>
+                    <input type="text" namn="shippingaddress" className='customer-form' />
+
+                    <label>Zipcode</label>
+                    <input type="text" namn="zipcode" className='customer-form' />
+
+                    <div className='payment-chart'>
+                      <h2 className='payment-title'>Payment</h2>
+                      <h3>Choose payment method in next step:</h3>
+                      <button type='button' className='place-order-btn'>Place order</button>
+                    </div>
+
+
+                </div>
+               
+       </div>
+    )  
+  }
 
   return (
-    <div>
-    {/*     <h1>checkout items</h1>
-        <p>här är mina varor:{checkoutItems}</p>
-        <p>du har {checkoutItems} st checkoutitems i korgen</p>
-        <button onClick={() => setCheckoutItems(product.title)}>lägg till</button> */}
+    <div className='checkout-wrapper'>
+        <div className='checkout-products'>
+            <h2 className='checkout-btn'>Checkout Page</h2>
+
+            {content}
+
+        </div>
     </div>
   );
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import '../Styling/product.css'
+import {Link} from 'react-router-dom'
 
-function ProductPage() {
+
+function ProductPage({addProducts}) {
 
   const [shoes, setProducts] = useState({});
    const params = useParams();
@@ -21,26 +23,34 @@ function ProductPage() {
     }
 
     useEffect(() => {
-        fetchData();
+      fetchData();
     }, []);
     
+    const handleClick = (shoes) => {
+      addProducts(shoes)
+    }
 
 
     return (
 
         <div className='shoe-items'>
+
             <article key={shoes.id}>
               <div className='shoe-info'>
                   <p className='shoe-title'>{'Our Bestseller - ' + shoes.title}</p>
                    <img className='shoe-img' src={shoes.url} alt='nike shoes' width={'50%'}/>
+                      
                       <div className='shoe-description'>
                         <p className='shoe-about'>{shoes.description}</p>
-                        <p className='shoe-price'>{'Cost: ' + shoes.price + ':-'}</p>
+                        <p className='shoe-price'>{'Cost: ' + shoes.price + ' SEK'}</p>
                         <p className='shoe-quantity'>{'Quantity: ' + shoes.storage + ' pairs'}</p>
+                        <button className='addproduct-button' onClick={() => handleClick(shoes)}>Add to cart</button> 
                       </div>
-                   <button className='addproduct-button'>Add to cart</button>
+                   
               </div>
             </article>
+            <Link to = '/checkout'>Checkout</Link>
+
         </div>
     )
   
