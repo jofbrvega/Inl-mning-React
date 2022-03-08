02a1a07b6fa9aea494a599c80b387399d9c29e59
Anna M Attehed <anna.attehed@medieinstitutet.se>
2022-03-08 12:20:34 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   BrowserRouter, Routes, Route,
 } from 'react-router-dom';
@@ -7,20 +7,41 @@ import Footer from './components/Footer';
 import ProductPage from './pages/Product';
 import Checkout from './pages/Checkout';
 
+
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  const addProducts = (newItem) => {
+
+    setItems ([
+      ...items,
+      newItem
+    ])
+    
+
+    
+    if (sum === 0) {
+      setSum(newItem.price)
+    } else {
+      setSum(sum + newItem.price)
+    }
+  }
   return (
     <div className="App">
 
        <BrowserRouter>
        
           <Routes>
-          <Route path='/'></Route>
-          <Route path='product/:id' element={<ProductPage />}></Route>
-          <Route path='/checkout' element={<Checkout />}></Route>
+          <Route path="/"></Route>
+          <Route path="product/:id" element={<ProductPage addProducts={addProducts} />}></Route>
+          <Route path="/checkout" element={<Checkout sum={sum} items={items} addProducts={addProducts} />}></Route>
           </Routes> 
           <Footer />
      </BrowserRouter> 
 
+    
     </div>
   );
 }
